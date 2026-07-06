bf89fafbdb60395e30fd44216b158dc2317fbd0e
arpitkumar20990 <arpitchaudharychhillar777@gmail.com>
2026-07-06 08:57:28 +0530

    updatedfile

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://food-view-pi.vercel.app",
+  "http://localhost:5173",];
 
 if (process.env.FRONTEND_URL) {
     allowedOrigins.push(process.env.FRONTEND_URL);
